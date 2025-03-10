8a50f6975638a2787594ab66d1e0152a224d4f1b
Baptiste DUFOUR <dufourbaptiste08@gmail.com>
2025-03-10 12:32:28 +0100

    Ajout du TransactionRepository

---
+++ Projet.Datas/Repositories/TransactionRepository.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Projet.Datas.Entities;
+using Projet.Datas;
+
+namespace Projet.Datas.Repositories
+{
+    public class TransactionRepository : IRepository<Transaction>
+    {
+        public TransactionRepository()
+        {
+            InitializeDatabase();
+        }
+
+        private void InitializeDatabase()
+        {
+            using var context = new MyDbContext();
+            context.Database.EnsureCreated();
+        }
+        public async Task<int> Add(Transaction entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Transaction> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Transaction GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Transaction entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
