1ff404e9f1ced6bc1a7a5c5f6efca89527668812
Thibaud DUFOUR <thibaud-dufour@sfr.fr>
2025-03-10 12:07:42 +0100

    Interface pour les repositories
    Co-Authored-By: Quentin-19 <199925967+Quentin-19@users.noreply.github.com>

--- Projet.Datas/Repositories/IRepository.cs
+++ Projet.Datas/Repositories/IRepository.cs
@@ -1,12 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Projet.Datas.Repositories
+﻿namespace Projet.Datas.Repositories
 {
-    interface IRepository
-    {
+    public interface IReadOnlyRepository<T>
+		where T : class
+	{
+		T GetById(int id);
+		IEnumerable<T> GetAll();
+	}
+
+	public interface IWriteOnlyRepository<T>
+		where T : class
+	{
+		void Add(T entity);
+		void Update(T entity);
+		void Delete(int id);
+	}
+
+	public interface IRepository<T> : IReadOnlyRepository<T>, IWriteOnlyRepository<T>
+		where T : class
+	{
+
     }
 }
