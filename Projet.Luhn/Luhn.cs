e94314778b08fc5de39fdbfca132df9d01a534f5
Baptiste DUFOUR <dufourbaptiste08@gmail.com>
2025-03-10 15:28:28 +0100

    Algorithme de Luhn

---
+++ Projet.Luhn/Luhn.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Specialized;
+using System.Net.Http.Headers;
+using System.Text.RegularExpressions;
+
+namespace Projet.Luhn
+{
+    public static class Luhn
+    {
+        public static bool IsValid(string CardNumber)
+        {
+            int sum = 0;
+
+            // Checks if the inputed Card number only contains numeric characters
+            if (!IsOnlyNumbers(CardNumber))
+            {
+                return false;
+            }
+
+            // The Luhn algorithm 
+            for (int i = 0; i < CardNumber.Length; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    int num = 2 * Int32.Parse(CardNumber[i].ToString());
+                    if ( num < 10)
+                    {
+                        sum += num;
+                    } else
+                    {
+                        sum += ( num - 9 );
+                    }
+                    
+                } else {
+                    int num = Int32.Parse(CardNumber[i].ToString());
+                    sum += num;
+                }                
+            }
+
+            return sum % 10 == 0;
+        }
+
+        private static bool IsOnlyNumbers(string CardNumber)
+        {
+            string regex = @"^\d+$";
+            return Regex.IsMatch(CardNumber,regex);
+        }
+    }
+}
