b9e8d1edec2e7157ea5ae1ad3e6ae69476ef3933
Thibaud DUFOUR <thibaud-dufour@sfr.fr>
2025-03-11 18:21:27 +0100

    Intégration de la connexion au projet sous la forme MVC

---
+++ Projet.Business/BankStatementDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace Projet.Business
+{
+	[XmlRoot("BankStatement")]
+	public class BankStatementDto
+	{
+		[XmlElement("AccountNumber")]
+		public string AccountNumber { get; set; }
+
+		[XmlElement("Period")]
+		public TransactionPeriodDto Period { get; set; }
+
+		[XmlArray("Transactions")]
+		[XmlArrayItem("Transaction")]
+		public List<TransactionDto> Transactions { get; set; }
+	}
+
+	public class TransactionPeriodDto
+	{
+		[XmlElement("StartDate")]
+		public DateTime StartDate { get; set; }
+
+		[XmlElement("EndDate")]
+		public DateTime EndDate { get; set; }
+	}
+}
--- Projet.Datas/Migrations/20250311163413_loginTable.Designer.cs
+++ Projet.Datas/Migrations/20250311163413_loginTable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Projet.Datas;
 
@@ -11,9 +12,11 @@ using Projet.Datas;
 namespace Projet.Datas.Migrations
 {
     [DbContext(typeof(MyDbContext))]
-    partial class MyDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250311163413_loginTable")]
+    partial class loginTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
