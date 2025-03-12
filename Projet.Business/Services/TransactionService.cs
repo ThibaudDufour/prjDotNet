89c83db18eb54680875ea34d0e34542d968f1dd8
Thibaud DUFOUR <thibaud-dufour@sfr.fr>
2025-03-12 13:49:15 +0100

    Mise à jour du solde pour nos clients

--- Projet.Business/Services/TransactionService.cs
+++ Projet.Business/Services/TransactionService.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using Microsoft.Identity.Client;
 using Projet.Datas.Entities;
 using Projet.Datas.Entities.Interfaces;
 using Projet.Datas.Repositories;
@@ -8,19 +9,58 @@ namespace Projet.Business.Services
 	public class TransactionService
 	{
 		private readonly TransactionRepository _transactionRepository;
+		private readonly BankCardRepository _bankCardRepository;
+		private readonly AccountRepository _accountRepository;
 		private readonly IMapper _mapper;
 
 		public TransactionService()
 		{
 			_transactionRepository = new TransactionRepository();
+			_bankCardRepository = new BankCardRepository();
+			_accountRepository = new AccountRepository();
 			_mapper = MappingConfig.Mapper;
 		}
 
+		/// <summary>
+		/// Ajoute une transaction et met à jour le solde du compte associé
+		/// </summary>
+		/// <param name="transDto"></param>
+		/// <returns></returns>
 		public async Task<int> AddTransaction(TransactionDto transDto)
         {
             Transaction trans = _mapper.Map<Transaction>(transDto);
-            return await _transactionRepository.Add(trans);
-        }
+            var result = await _transactionRepository.Add(trans);
+
+			// Si la transaction a été ajoutée, modifier le solde du compte
+			if (result > 0)
+			{
+				// Récupère la carte bancaire associée à la transaction
+				BankCard? card = await _bankCardRepository.GetByCardNumber(transDto.CardNumber);
+				if (card != null)
+				{
+					// Récupère le compte associé à la carte bancaire
+					Account? account = await _accountRepository.GetByAccountNumber(card.AccountNumber);
+
+					// Si le compte existe et que la transaction est un dépôt d'argent
+					if (account != null && transDto.TransactionType == EnumTransactionType.CashDeposit)
+					{
+						// Dépose le montant sur le compte
+						account.Deposit(transDto.Amount);
+						await _accountRepository.Update(account);
+					}
+					else if (account != null)
+					{
+						// Retire le montant du compte
+						if (account.Withdraw(transDto.Amount))
+						{
+							await _accountRepository.Update(account);
+						}
+					}
+				}
+			}
+
+			return result;
+		}
 
 		/// <summary>
 		/// Récupère les transactions d'un compte pour une période donnée
