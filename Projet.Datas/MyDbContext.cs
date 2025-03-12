b3511089777502640ea14fc09ff9968b6c706b48
Thibaud DUFOUR <thibaud-dufour@sfr.fr>
2025-03-12 10:30:06 +0100

    Modifications des données lors de l'insertion de celles-ci en BDD

--- Projet.Datas/MyDbContext.cs
+++ Projet.Datas/MyDbContext.cs
@@ -262,11 +262,11 @@ namespace Projet.Datas
 				.HasData(
 					new Transaction { Id = 1, CardNumber = "4974018502231235", Amount = 150.75, TransactionType = EnumTransactionType.CashDeposit, TransactionDate = new DateTime(2023, 5, 10), Currency = EnumCurrency.EUR },
 					new Transaction { Id = 2, CardNumber = "4974018502238270", Amount = 75.50, TransactionType = EnumTransactionType.CashWithdrawal, TransactionDate = new DateTime(2023, 6, 15), Currency = EnumCurrency.USD },
-					new Transaction { Id = 3, CardNumber = "4974018502233220", Amount = 220.00, TransactionType = EnumTransactionType.CardPayment, TransactionDate = new DateTime(2023, 7, 20), Currency = EnumCurrency.EUR },
+					new Transaction { Id = 3, CardNumber = "4974018502233920", Amount = 220.00, TransactionType = EnumTransactionType.CardPayment, TransactionDate = new DateTime(2023, 7, 20), Currency = EnumCurrency.EUR },
 					new Transaction { Id = 4, CardNumber = "4974018502234671", Amount = 50.00, TransactionType = EnumTransactionType.CashDeposit, TransactionDate = new DateTime(2023, 8, 5), Currency = EnumCurrency.GBP },
 					new Transaction { Id = 5, CardNumber = "4974018502235218", Amount = 300.00, TransactionType = EnumTransactionType.CashWithdrawal, TransactionDate = new DateTime(2023, 9, 1), Currency = EnumCurrency.EUR },
 					new Transaction { Id = 6, CardNumber = "4974018502236345", Amount = 125.25, TransactionType = EnumTransactionType.CardPayment, TransactionDate = new DateTime(2023, 10, 10), Currency = EnumCurrency.USD },
-					new Transaction { Id = 7, CardNumber = "4974018502233220", Amount = 400.00, TransactionType = EnumTransactionType.CashDeposit, TransactionDate = new DateTime(2023, 11, 12), Currency = EnumCurrency.EUR },
+					new Transaction { Id = 7, CardNumber = "4974018502237422", Amount = 400.00, TransactionType = EnumTransactionType.CashDeposit, TransactionDate = new DateTime(2023, 11, 12), Currency = EnumCurrency.EUR },
 					new Transaction { Id = 8, CardNumber = "4974018502238539", Amount = 20.00, TransactionType = EnumTransactionType.CashWithdrawal, TransactionDate = new DateTime(2023, 12, 20), Currency = EnumCurrency.GBP },
 					new Transaction { Id = 9, CardNumber = "4974018502239656", Amount = 180.75, TransactionType = EnumTransactionType.CardPayment, TransactionDate = new DateTime(2024, 1, 5), Currency = EnumCurrency.EUR },
 					new Transaction { Id = 10, CardNumber = "4974018502230782", Amount = 99.99, TransactionType = EnumTransactionType.CashDeposit, TransactionDate = new DateTime(2024, 2, 15), Currency = EnumCurrency.USD }
